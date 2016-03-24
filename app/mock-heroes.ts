761200a29efd066fffba88494178ca4813a93cdb
quitat <lorenaquitat540@gmail.com>
2016-03-24 23:40:58 +0800

    new

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export var HEROES: Hero[] = [
+    {"id": 11, "name": "Batman"},
+    {"id": 12, "name": "Superman"},
+    {"id": 13, "name": "Carrotman"},
+    {"id": 14, "name": "Cabbageman"},
+    {"id": 15, "name": "Dolpman"},
+    {"id": 16, "name": "RubberMan"},
+    {"id": 17, "name": "Lastikman"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
